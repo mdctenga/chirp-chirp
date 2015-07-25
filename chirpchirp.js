7ebce4a2440e8cf9d8e58ceed03438e1d07847f6
mdctenga <mdctenga@gmail.com>
2015-07-25 12:14:42 -1000

    Implement tests

---
+++ chirpchirp.js
@@ -0,0 +1,5 @@
+module.exports = chirpderp;
+
+function chirpderp (){
+  return 1;
+}
--- test/test-script.js
+++ test/test-script.js
@@ -1,3 +1,9 @@
 var chai = require('chai');
 var should = chai.should();
+var chirp = require('../chirpchirp.js');
 
+describe('chirp', function () {
+  it('should be a function', function () {
+    chirp.should.be.a('function');
+  });
+});
